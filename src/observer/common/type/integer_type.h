86d0f43f262ad93785ec9679503c65aaf64b00e8
Soulter <905617992@qq.com>
2024-10-30 20:36:05 +0800

    perf: 补充了float、int类型的类型转换 cast_to()

--- src/observer/common/type/integer_type.h
+++ src/observer/common/type/integer_type.h
@@ -30,6 +30,8 @@ public:
   RC negative(const Value &val, Value &result) const override;
   RC max(const Value &left, const Value &right, Value &result) const override;
   RC min(const Value &left, const Value &right, Value &result) const override;
+  
+  RC cast_to(const Value &val, AttrType type, Value &result) const override;
 
   RC set_value_from_str(Value &val, const string &data) const override;
 
