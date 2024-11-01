55a4000a2a2db77f385d76f9824acfb9d4d97a75
Nelson Boss <bosswnx@outlook.com>
2024-11-02 00:40:13 +0800

    🐞 fix: 修复 make_key rid 复制错误

--- src/observer/storage/table/table_meta.cpp
+++ src/observer/storage/table/table_meta.cpp
@@ -169,7 +169,7 @@ const IndexMeta *TableMeta::find_index_by_fields(const std::vector<const char *>
     bool found = true;
     for (int i = 0; i < index.field_metas().size(); i++) {
       // b+tree 的索引字段是有顺序的，所以直接按顺序比较
-      if (index.field_metas()[i].name() != field_names[i]) {
+      if (strcmp(index.field_metas()[i].name(), field_names[i]) != 0) {
         found = false;
         break;
       }
